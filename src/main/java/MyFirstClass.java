ac110e2273a4e454e6d3999ce83f690096c22fcb
anton-aleksandrov <aa08131979@gmail.com>
2020-03-19 10:22:32 -0400

    .

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -1,5 +1,15 @@
 public class MyFirstClass {
+
+    public static int Sum (int a, int b){
+        int sum = 0;
+        sum = a+b;
+        return sum;
+    }
+
  public static void main(String[] args) {
+       int a = 2;
+       int b = 3;
+     System.out.println(Sum (a,b));
      System.out.println("Hello World");
 }
 }
